acd444301c0ff4fbdb81fab9345ec16a1190db35
Sathwik <112880525+Sathwik656@users.noreply.github.com>
2025-11-14 18:41:51 +0530

    lamda

--- Functions/lamda.py
+++ Functions/lamda.py
@@ -7,4 +7,15 @@ def get_multiples(n):
 def get_multiples_of_five(n):
     return list(filter(lambda k: not k%5, range(n)))
 
-    
+map(lambda *a: a, range(3))
+
+list(map(lambda *a: a, range(3)))
+
+list(map(lambda *a: a, range(3), 'abc'))
+
+list(map(lambda *a: a, range(3), 'abc', range(4,7)))
+
+list(map(lambda *a: a, (), 'abc'))
+
+list(map(lambda *a: a, (1,2), 'abc'))
+
